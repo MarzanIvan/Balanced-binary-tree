7196f82c7c5fed416f1205b2d384e3f137743bf7
MarzanIvan <boss.marzan@bk.ru>
2024-05-04 12:48:45 +0300

    create empty project

---
+++ map.cxx
@@ -0,0 +1,9 @@
+//
+// Created by Ioann Marzan on 04.05.2024.
+//
+
+#include "map.hxx"
+
+namespace custom {
+
+} // custom
---
+++ map.hxx
@@ -0,0 +1,16 @@
+//
+// Created by Ioann Marzan on 04.05.2024.
+//
+
+#ifndef BALANCED_BINARY_TREE_MAP_HXX
+#define BALANCED_BINARY_TREE_MAP_HXX
+
+namespace custom {
+
+    class map {
+
+    };
+
+} // custom
+
+#endif //BALANCED_BINARY_TREE_MAP_HXX
